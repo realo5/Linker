0d823a84ad109f21c24d4b4a2b98680a1cf7adbe
Raf Richards <nightbreeze714@hotmail.com>
2016-05-28 00:52:00 -0400

    Finished Todo list functions

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,12 +1,13 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create
+    ]
   def create
     @todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
     redirect_to @todo_list
   end
   
   def destroy
-    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
       flash[:success] = "Todo List item was deleted."
     else
@@ -14,12 +15,22 @@ class TodoItemsController < ApplicationController
     end
     redirect_to @todo_list
   end
+  
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed!"
+  end
+  
+  end
+  
   private
   
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
   end
   
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   def todo_item_params
     params[:todo_item].permit(:content)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528034146) do
+ActiveRecord::Schema.define(version: 20160528041234) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160528034146) do
     t.integer  "todo_list_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
