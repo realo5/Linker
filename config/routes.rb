d95628796c64705ac980d4862c16e5070110ef35
Raf Richards <nightbreeze714@hotmail.com>
2016-05-28 00:10:35 -0400

    Added Todo List & Items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items
+  end
   resources :comments
   
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528022736) do
+ActiveRecord::Schema.define(version: 20160528034146) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -33,6 +33,22 @@ ActiveRecord::Schema.define(version: 20160528022736) do
   end
 
   add_index "links", ["user_id"], name: "index_links_on_user_id"
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "todo_list_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
